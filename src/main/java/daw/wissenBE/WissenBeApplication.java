230fa68c7b98c25e2b7857448cf127e85a82c905
F2A4 <fabi.linder@gmail.com>
2021-10-23 19:59:32 +0200

    Initial commit

---
+++ src/main/java/daw/wissenBE/WissenBeApplication.java
@@ -0,0 +1,13 @@
+package daw.wissenBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WissenBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WissenBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daw/wissenBE/WissenBeApplicationTests.java
@@ -0,0 +1,13 @@
+package daw.wissenBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WissenBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
